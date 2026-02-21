59ad433f618511c514422b1942b81f1a48e20bdb
Yanfei Yu <3155021Y@student.gla.ac.uk>
2026-02-21 01:56:29 +0000

    Fix some errors and add another human win notification

--- app/events/Initalize.java
+++ app/events/Initalize.java
@@ -10,6 +10,7 @@ import akka.actor.ActorRef;
 import commands.BasicCommands;
 import structures.GameState;
 import structures.basic.Board;
+import structures.basic.Card;
 import structures.basic.Player;
 import structures.basic.Tile;
 import structures.basic.Unit;
@@ -56,7 +57,7 @@ public class Initalize implements EventProcessor {
         gs.player2.setMana(gs.aiCurrentMana);
 
         gs.clearSelectionAndHighlights();
-
+        
         // Board model + basic UI draw
         gs.board = new Board(BOARD_WIDTH, BOARD_HEIGHT);
         for (int x = 0; x < BOARD_WIDTH; x++) {
@@ -73,9 +74,41 @@ public class Initalize implements EventProcessor {
 
         gs.aiAvatar = spawnAvatar(out, gs, AI_AVATAR_ID, AI_AVATAR_X, AI_AVATAR_Y,
                 "conf/gameconfs/avatars/avatar2.json", 20, 2);
+        
+        // load all cards into player1's and player2's deck
+        List<Card> deck1=OrderedCardLoader.getPlayer1Cards(1);
+        gs.player1.getDeck().addAll(deck1);
+
+        List<Card> deck2=OrderedCardLoader.getPlayer2Cards(1);
+        gs.player2.getDeck().addAll(deck2);
+
+        // draw 3 inititial card into player's hand
+        for (int i=0; i<3;i++){
+            if(gs.player1.getDeck().isEmpty()){
+            // if deck is empty, player lose the game
+                gs.gameOver=true;
+                gs.winner="player2";
+                BasicCommands.addPlayer1Notification(out,"Sorry, your deck is empty. You lose the game.", 2);
+                break;
+            }
+            if(gs.player2.getDeck().isEmpty()){
+                gs.gameOver=true;
+                gs.winner="player1";
+                BasicCommands.addPlayer1Notification(out,"Congratulation!! You win the game.", 2)
+                break;
+            }
+            // draw the first card of the deck into hand
+            else{
+            Card topCard=gs.player1.getDeck().remove(0);
+            gs.player1.getHand().add(topCard);
+            BasicCommands.drawCard(out, topCard, i + 1, 0);
+            // AI also draw card but don't need visualize.
+            Card topCardAI=gs.player2.getDeck().remove(0);
+            gs.player2.getHand().add(topCardAI);
+            }}
 
         // Mark initialised (оставляю как у тебя — часто тесты завязаны на это поле)
-        gs.gameInitalised = true;
+        gs.gameInitalised = true;   
     }
 
     private Unit spawnAvatar(ActorRef out, GameState gs, int id, int x, int y, String conf, int hp, int atk) {
@@ -99,40 +132,11 @@ public class Initalize implements EventProcessor {
 
         return avatar;
     }
-
+    
     private void sleep(int ms) {
         try { Thread.sleep(ms); } catch (Exception e) {}
     }
-
-    // load all cards into player1's and player2's deck
-    List<Card> deck1=OrderedCardLoader.getPlayer1Cards(1);
-    gs.player1.getDeck().addAll(deck1);
-
-    List<Card> deck2=OrderedCardLoader.getPlayer1Cards(1);
-    gs.player2.getDeck().addAll(deck2);
-
-    // draw 3 inititial card into player's hand
-    for (int i=0; i<3;i++){
-        if(gs.player1.getDeck().isEmpty()){
-            // if deck is empty, player lose the game
-            gs.gameOver=True;
-            gs.winner=gs.player2;
-            BasicCommands.addPlayer1Notification(out, "Sorry, your deck is empty. You lose the game.", 2);
-            break;
-        }
-        if(gs.player2.getDeck().isEmpty()){
-            gs.gameOver=True;
-            gs.winner=gs.player1;
-            break;
-        }
-    // draw the first card of the deck into hand
-        else{
-        Card topCard=gs.player1.getDeck().remove(0)
-        gs.player1.getHand().add(topCard);
-        BasicCommands.drawCard(out, topCard, i + 1, 0);
-
-        Card topCardAI=gs.player2.getDeck().remove(0)
-        gs.player2.getHand().add(topCardAI);
+    
     }
-    }
+        
 }
